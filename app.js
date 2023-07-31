57bc518c2041323b4b498f7b0c74e523e58159bb
ahmedjp143 <ajb1434@gmail.com>
2023-07-31 15:20:57 +0100

    coomit sesdaxaad

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 app.get('/', (req, res) => {
   res.json('hi xasan');
 });
